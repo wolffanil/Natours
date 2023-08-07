a9285f79c0262befc3407e17a99d418657da8fd6
wolffanil2 <wolffanil@mail.ru>
2023-08-07 21:00:53 +0400

    tired3

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ const bookingRouter = require('./routes/bookingRouter');
 
 const app = express();
 
-app.enable('trust proxy');
+// app.enable('trust proxy');
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
