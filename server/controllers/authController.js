06be340ed55b7e504fa3fd27c5d572064a7c7089
wolffanil2 <wolffanil@mail.ru>
2023-08-07 20:54:31 +0400

    tired2

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -43,7 +43,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   });
 
   const url = `${req.protocol}://${req.get('host')}/me`;
-  await new Email(newUser, url).sendWelcome();
+  // await new Email(newUser, url).sendWelcome();
 
   createSendToken(newUser, 201, res, req);
 });
